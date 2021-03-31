a6604f72b697f4d2efe8c70a2e5e66e8db91045e
Bitopan Deka <bitopanxon1@gmail.com>
2021-03-31 03:57:50 -0700

    Assignment Level-1

--- block-BJaadg/code/index.js
+++ block-BJaadg/code/index.js
@@ -1,21 +1,22 @@
 // After knowing about string methods, practice those by solving problems given below.
 
-let from = 'Syrio Forel';
-let quote = 'There is only one thing we say to death: Not today';
-let to = 'Arya Stark';
+let from = "Syrio Forel";
+let quote = "There is only one thing we say to death: Not today";
+let to = "Arya Stark";
 
 /*
 1. Find the index of the first 'is' in the variable quote. And store it in a new variable named indexOfIs
 */
+let indexOfIs = quote.indexOf("is");
 
 /*
 2. Find the character at the index indexOfIs (Problem 1) in quote.
 */
-
+quote.charAt(indexOfIs);
 /*
 3. Log the message saying `The index of first is in quote is 7`
 */
-
+console.log(`The index of first is in quote is ${indexOfIs}`);
 /*
 4. Log the message for first 6 characters of quote like this.
   The character at index 0 is 'T'
@@ -25,35 +26,44 @@ let to = 'Arya Stark';
   The character at index 4 is 'e'
   The character at index 5 is ' '
 */
-
+let final = quote.slice(0, 7).split("");
+final.forEach((letter, index) =>
+  console.log(`The character at ${index} is  ${letter}`)
+);
 /*
 5. Using the variable from , to and quote variable dispaly this message
   "Syrio Forel said There is only one thing we say to death: Not today to Arya Stark." (use concat method)
 */
-
+let result = from.concat(" " + quote + " " + to);
+console.log(result);
 /*
 6. Does from, to and quote ends with "rk". Check all three.
 */
-
+console.log(from.endsWith("rk"));
+console.log(to.endsWith("rk"));
+console.log(quote.endsWith("rk"));
 /*
 7. Does quote includes the word "Only"
 */
-
+console.log(quote.includes("Only"));
 /*
 8. Does quote includes the word " we"
 */
-
+console.log(quote.includes("we"));
 /*
 9. Find the index of the the word `we` in quote
 */
-
+console.log(quote.indexOf("we"));
 /*
 10. Split the quote into individual word and store it in a variable name quoteSplitted
 */
-
+let quoteSplitted = quote.split(" ");
+console.log(quoteSplitted);
 /*
 11. Change the word "today" in quoteSplitted to "tomorrow" and join all the words to form a sentance.
 */
+let quoteSplitt = quoteSplitted.splice(quoteSplitted.length - 1, 1, "tomorrow");
+console.log(quoteSplitt);
 
 /*
 12. Find the index of second "o" in quote. Use indexOf
